c6643298a307e03ad3826039ee86ac06f1c2e39e
Timichae <timmichaels@tekserves-MacBook-Air.local>
2017-08-24 21:37:57 -0400

    revised localizer for RH's review

--- localizer_psychopy/localizer_exp_TIM.py
+++ localizer_psychopy/localizer_exp_TIM.py
@@ -2,10 +2,6 @@
 from psychopy import visual, core, event, sound, gui, data, logging
 # import math (for rounding function)
 import math
-from psychopy.iohub import launchHubServer
-from psychopy.iohub.devices.keyboard import KeyboardInputEvent
-io=launchHubServer()
-keyboard=io.devices.keyboard
 
 #set parent directory
 parent_dir = "./"
@@ -17,6 +13,12 @@ win = visual.Window(size = [1920,1080],
                     units = "pix")
 
 # Set up text displays
+ScannerWait_txt = visual.TextStim(win, text = "Waiting for scanner....",
+                        pos = [0,0],
+                        color = "black",
+                        height = 50,
+                        font = "Arial")
+
 Rhand_txt = visual.TextStim(win, text = "Curl your right hand.",
                         pos = [0,0],
                         color = "black",
@@ -62,60 +64,50 @@ lastLog = logging.LogFile("lastRun.log", level=logging.INFO, filemode='w')
 STIM = {1: Rhand_txt, 2: Lhand_txt, 3: Rfoot_txt, 4: Lfoot_txt, 5: tongue_txt, 6: null_txt}
 # load in our stimulus timing csv file
 TRIAL_LIST = data.importConditions(fileName = parent_dir + "localizer_datasource.csv")
-#defining get_keypress to use as esc from experiment 
-def get_keypress():
-    keys = event.getKeys()
-    if keys:
-        return keys[0]
-    else:
-        return None
-def shutdown():
-    win.close()
-    core.quit()
 
 #create clock
 globalClock = core.Clock()
+#show waiting for scanner until keypress
+ScannerWait_txt.draw()
+win.flip()  
 #wait for a 5 or t keypress to start
-print "Waiting for scanner..."
-key = get_keypress ()
-if key == 't' or '5' 
-    keyboard.waitForKeys() = False 
-else:
-    keyboard.waitForKeys() = True 
+def get_keypress():
+    key = event.getKeys()
+key= get_keypress()
+key==event.waitKeys(keyList=['t','5'])
 # Create variable for start point from when scanner began (t0)
 t0 = globalClock.getTime()
 #creating variable for flip duration
-flip_duration= 20
-# create a variable for the time (t1)
-t1=globalClock.getTime()-t0
-
+flip_duration= 21
 # while the clock is running...
-while globalClock.getTime()-t0 < 503.25:
+while globalClock.getTime()-t0 < 504.00:
 # Goal for this section: Check what time it is. If the time matches a value in the column "Time"
 # then present the text display from that same row for 1.25 seconds and then go back to blank
 # if  the time is greater than 503.25 seconds, end the while loop
+# create a variable for the time (t1)
+    t1=globalClock.getTime()-t0
 #setting up the first stimulus outside  the loop
-    Rhand_txt.draw()
-    null_txt.draw() 
+    STIM[TRIAL_LIST[0]["StimType"]].draw()
 # look through the rows of the data source     
     for index in range(len(TRIAL_LIST)):
-# if the number in the "Time" column matches the timestamp (rounded down to nearest integer)
-        while True TRIAL_LIST[index]["Time"] >= math.floor(t1) :
+# if the number in the "Time" column is less than the timestamp
+        while t1 <= TRIAL_LIST[index+1]["Time"]:
 # draw the screen indicated in the StimType column and then present it
-            STIM[TRIAL_LIST[index]["StimType"]].win.flip()
+            if TRIAL_LIST[index]["Time"] <= t1:
+# draw the screen indicated in the StimType column and then present it
+                STIM[TRIAL_LIST[index]["StimType"]].draw(); win.flip()
 # wait for 1.25 seconds
-            core.wait(1.25)
+                core.wait(1.25)
 # then flip back to crosshair
-            null_txt.flip() 
-#while current stimulus is presented, draw next stimulus 
-        while True TRIAL_LIST[index]["Time"] >= math.floor(t1)+flip_duration :
-            STIM[TRIAL_LIST[index]["StimType"]].draw() 
+                null_txt.draw()
+                win.flip()
 #if esc key pressed, abort the task
-        keys = get_keypress()
-        if keys[0] == 'Esc' :
-                shutdown() 
-        elif globalClock.getTime()-t0 > 503.25:
-            break
+                key= get_keypress()
+            elif key==event.getKeys(keyList=['escape']):
+                win.close()
+                core.quit()
+            elif globalClock.getTime()-t0 > 503.25:
+                break
 
 # close everything
 win.close()
